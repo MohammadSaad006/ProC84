8d9b8580b2fbf280bd9c2fcaa1e574bb075819f5
MohammadSaad006 <73736644+MohammadSaad006@users.noreply.github.com>
2021-08-24 17:48:12 +0530

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+export default class Profile extends React.Component{
+  render(){
+    return(
+      <View><Text>Profile Screen</Text></View>
+    )
+  }
+}
